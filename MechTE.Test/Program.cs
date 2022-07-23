f0eba487bbf04d71688c6d486da6ad60d7cf94bf
ouyangkai <843766158@qq.com>
2022-07-23 10:38:13 +0800

    1

---
+++ MechTE.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using MechTE;
+using MechTE.TWinForm;
+using System;
+using System.Diagnostics;
+using System.IO;
+using DotNet.Utilities;
+using MechTE.TZip;
+
+namespace MechTE.Test
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //TForm.TGetProcessesByName("MechTE.Test");
+            //TCmd.TCmd.TCmds("notepad");
+            // TCmd.TCmd.ExeCommand("notepad");
+            //var R=  TFile.TFile.IsExistDirectory(@"D:\sw\Console\MechTE\MechTE\bin\DebugS");
+            //  TIni.TIni.TGetIniAll();
+            //    TIni.TIni. TDelIniValue(@".\CONFIG.ini","CONFIG","Delay1");
+            // TIni.TIni.TDelIniSection(@".\CONFIG.ini","DONGLE1");
+            //var d=  TFile.TFile.TQueryFile(@"D:\sw\type_sw\19.HDT657\Debug-SMT");
+
+            // var r= TFile.TFile.GetFileNames(@"D:\sw\Console\MechTE\MechTE\bin\Debug");
+            //  TIni.TIni.TGetIniAll();
+            //var r=  StringCAPTCHA.Number(1);
+
+            
+            // TZips.TZipDirectory(@"C:\Users\ch190006\Desktop\新增資料夾 (2)", @"C:\Users\ch190006\Desktop\新增資料夾 (2)\123.zip");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ MechTE/MechDll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MechTE;
+using MechTE.Cmd;
+
+namespace MechTE
+{
+    /// <summary>
+    /// 调试模块
+    /// </summary>
+    public class MechDll
+    {
+        private string m_strPara;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public MechDll(string strPara)
+        {
+            this.m_strPara = strPara;
+        }
+        /// <summary>
+        /// 开始测试
+        /// </summary>
+        /// <returns></returns>
+        public string Run(string name)
+        {
+            string[] str = name.Split('-');
+            switch (str[0])
+            {
+                case"cmd" :
+                    return TCmd.ExeCommand(str[1]);
+                default:
+                    break;
+            }
+            return "Run False";
+        }
+    }
+}
