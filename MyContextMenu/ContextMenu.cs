c96985aaaf37e19396dea0d19713bda4d6eaed05
m-Kai.Ouyang <843766158@qq.com>
2023-07-06 17:00:04 +0800

    feat:新增个人右键功能

--- MyContextMenu/ContextMenu.cs
+++ MyContextMenu/ContextMenu.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
 
-namespace MechTE_Shell
+namespace MyContextMenu
 {
     [ComVisible(true)]
     //如果按文件类型，按以下设置
@@ -18,7 +18,7 @@ namespace MechTE_Shell
     //设置对全部文件和目录可用
     [COMServerAssociation(AssociationType.AllFiles), COMServerAssociation(AssociationType.Directory)]
 
-    public class ArrContextMenu : SharpContextMenu
+    public class ContextMenu : SharpContextMenu
     {
         /// <summary>
         /// 判断菜单是否需要被激活显示
@@ -49,16 +49,12 @@ namespace MechTE_Shell
 
             
             //设置次级菜单
-            Dictionary<string, string> subItemsInfo = new Dictionary<string, string>()
+            var subItemsInfo = new Dictionary<string, string>()
             {
-                { "上传(工程)", "EngineeringMode.exe,uploadingEng" },
-                { "下载(工程)", "EngineeringMode.exe,downloadEng" },
-                { "SimpleHIDWrite", "EngineeringMode.exe,SimpleHIDWrite" },
-                { "更新", "EngineeringMode.exe,programUpdate" },
-                // { "卸载", "EngineeringMode.exe,unload" }
+                { "清理以太网", "EngineeringMode.exe,Ethernet" },
             };
             
-            foreach (KeyValuePair<string,string> kv in subItemsInfo)
+            foreach (var kv in subItemsInfo)
             {
                 var subItem = new ToolStripMenuItem(kv.Key);
                 subItem.Click += (o, e) =>
@@ -78,23 +74,23 @@ namespace MechTE_Shell
         public void Item_Click(object sender, EventArgs e, string arg)
         {
             //分割,文件名和传递参数
-            string[] argStrings = arg.Split(',');
+            var argStrings = arg.Split(',');
             var fileName = argStrings[0];
             var identify = argStrings[1];
             
             //获取当前dll所在路径
-            string rootPath = getRootPath();
+            var rootPath = getRootPath();
             //文件路径+文件名称组合
-            string appFile = $@"{rootPath}\{fileName}";
+            var appFile = $@"{rootPath}\{fileName}";
             if (!File.Exists(appFile))
             {
                 MessageBox.Show($"找不到程序路径:{Environment.NewLine}{appFile}", "出错了", MessageBoxButtons.OK);
                 return;
             }
             //转换为列表，然后将fileName添加到列表中
-            List<string> paths = SelectedItemPaths.ToList();
+            var paths = SelectedItemPaths.ToList();
             paths.Add(fileName);
-            string args = string.Join(" ", paths);
+            var args = string.Join(" ", paths);
             Process.Start(appFile, args + " "+identify);        
         }
 
@@ -102,11 +98,11 @@ namespace MechTE_Shell
         public string getRootPath()
         {
             // 获取当前程序集的代码基路径
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
             // 创建一个UriBuilder对象，用于解析代码基路径
-            UriBuilder uri = new UriBuilder(codeBase);
+            var uri = new UriBuilder(codeBase);
             // 获取解析后的路径，并对路径中的特殊字符进行解码
-            string path = Uri.UnescapeDataString(uri.Path);
+            var path = Uri.UnescapeDataString(uri.Path);
             // 获取解析后的路径，并对路径中的特殊字符进行解码
             return Path.GetDirectoryName(path);
         }
