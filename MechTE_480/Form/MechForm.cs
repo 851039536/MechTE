60f6c5207aee73dfc4cb7ca84f951f4ede01ac9c
m-Kai.Ouyang <843766158@qq.com>
2023-10-23 14:40:55 +0800

    fix:新增控件大小随窗体大小等比例缩放函数

--- MechTE_480/Form/MechForm.cs
+++ MechTE_480/Form/MechForm.cs
@@ -10,6 +10,61 @@ namespace MechTE_480.Form
     public partial class MechForm
     {
         
+        #region 控件大小随窗体大小等比例缩放
+        /// <summary>
+        /// 定义当前窗体的宽度
+        /// </summary>
+        public static float X;
+        /// <summary>
+        /// 定义当前窗体的高度
+        /// </summary>
+        public static float Y;
+        /// <summary>
+        /// 控件大小随窗体大小等比例缩放,
+        /// 在窗体重载中使用 >>  MechForm.X = this.Width; MechForm.Y = this.Height;  MechForm.SetTag(this);
+        /// </summary>
+        /// <param name="cons"></param>
+        public static void SetTag(Control cons)
+        {
+            foreach (Control con in cons.Controls) {
+                con.Tag = con.Width + ";" + con.Height + ";" + con.Left + ";" + con.Top + ";" + con.Font.Size;
+                if (con.Controls.Count > 0) {
+                    SetTag(con);
+                }
+            }
+        }
+        /// <summary>
+        /// 设置缩放,在Resize事件中使用 >>
+        /// float newX = this.Width / MechForm.X;
+        /// float newY = this.Height / MechForm.Y;
+        /// MechForm.SetControls(newX,newY,this);
+        /// </summary>
+        /// <param name="newx">X轴</param>
+        /// <param name="newy">Y轴</param>
+        /// <param name="cons"></param>
+        public static void SetControls(float newx,float newy,Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls) {
+                //获取控件的Tag属性值，并分割后存储字符串数组
+                if (con.Tag != null) {
+                    string[] mytag = con.Tag.ToString().Split(new char[] { ';' });
+                    //根据窗体缩放的比例确定控件的值
+                    con.Width = Convert.ToInt32(System.Convert.ToSingle(mytag[0]) * newx);//宽度
+                    con.Height = Convert.ToInt32(System.Convert.ToSingle(mytag[1]) * newy);//高度
+                    con.Left = Convert.ToInt32(System.Convert.ToSingle(mytag[2]) * newx);//左边距
+                    con.Top = Convert.ToInt32(System.Convert.ToSingle(mytag[3]) * newy);//顶边距
+                    Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字体大小
+                    con.Font = new Font(con.Font.Name,currentSize,con.Font.Style,con.Font.Unit);
+                    if (con.Controls.Count > 0) {
+                        SetControls(newx,newy,con);
+                    }
+                }
+            }
+        }
+        #endregion
+        
+        
         /// <summary>
         /// 默认弹框提示(确认/取消)
         /// </summary>
