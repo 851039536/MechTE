65b20ae78f5e0a09f3190d8c01644b551fa1cfc3
m-Kai.Ouyang <843766158@qq.com>
2023-11-29 18:27:38 +0800

    fix:指定debug加入管控

---
+++ MechTE_480/bin/Debug/merge.bat
@@ -0,0 +1,1 @@
+cmd /k "D:\sw\编程资料\Dll合并工具\ILMerge.exe /ndebug /target:dll /out:D:\sw\class_library\MechTE\MechTE_480\bin\Debug\MechTE_480_Merge.dll /log D:\sw\class_library\MechTE\MechTE_480\bin\Debug\merge\MySql.Data.dll D:\sw\class_library\MechTE\MechTE_480\bin\Debug\merge\RestSharp.dll D:\sw\class_library\MechTE\MechTE_480\bin\Debug\MechTE_480.dll"
